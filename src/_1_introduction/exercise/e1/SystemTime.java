a992162548cc18490435d1c368c0edd75f29b041
Đoàn Đình Vũ <vu@gmail.com>
2022-06-01 18:25:15 +0700

    m

---
+++ src/_1_introduction/exercise/e1/SystemTime.java
@@ -0,0 +1,9 @@
+package _1_introduction.exercise.e1;
+
+import java.util.Date;
+
+public class SystemTime {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+    }
+}
